03f7c5f88ec9c886c19f4471e2cc34f06b776204
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2022-07-27 14:42:45 +0200

    add condition field

--- server/db/v1.0.0.sql
+++ server/db/v1.0.0.sql
@@ -226,3 +226,21 @@ INSERT IGNORE INTO `hooks` (`id_hookTypes`, `name`, `description`, `class`, `fun
 
 -- register hook get_csp_rules
 INSERT IGNORE INTO `hooks` (`id_hookTypes`, `name`, `description`, `class`, `function`, `exec_class`, `exec_function`) VALUES ((SELECT id FROM lookups WHERE lookup_code = 'hook_overwrite_return' LIMIT 0,1), 'qualtrics-addCspRule', 'Add csp rule for Qualtrics iframe', 'BasePage', 'getCspRules', 'QualtricsHooks', 'setCspRules');
+
+-- add field condtion to all styles that have css field
+INSERT IGNORE INTO `styles_fields` (`id_styles`, `id_fields`, `help`)
+SELECT style_id, get_field_id('condition'), 'The field `condition` allows to specify a condition. Note that the field `condition` is of type `json` and requires\n1. valid json syntax (see https://www.json.org/)\n2. a valid condition structure (see https://github.com/jwadhams/json-logic-php/)\n\nOnly if a condition resolves to true the sections added to the field `children` will be rendered.\n\nIn order to refer to a form-field use the syntax `"@__form_name__#__from_field_name__"` (the quotes are necessary to make it valid json syntax) where `__form_name__` is the value of the field `name` of the style `formUserInput` and `__form_field_name__` is the value of the field `name` of any form-field style.' 
+FROM view_style_fields
+WHERE field_name = 'css' and style_name <> 'conditionalContainer';
+
+-- add field jquery_builder_json to all styles that have css field
+INSERT IGNORE INTO `styles_fields` (`id_styles`, `id_fields`, `help`)
+SELECT style_id, get_field_id('jquery_builder_json'), 'This field contains the JSON structure for the jquery builder. The field should be hidden' 
+FROM view_style_fields
+WHERE field_name = 'css' and style_name <> 'conditionalContainer';
+
+-- add field debug to all styles that have css field
+INSERT IGNORE INTO `styles_fields` (`id_styles`, `id_fields`, `help`)
+SELECT style_id, get_field_id('debug'), 'If *checked*, debug messages will be rendered to the screen. These might help to understand the result of a condition evaluation. **Make sure that this field is *unchecked* once the page is productive**.' 
+FROM view_style_fields
+WHERE field_name = 'css' and style_name <> 'conditionalContainer' and style_name <> 'autocomplete';
