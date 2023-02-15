2dac39f787b927ad8bbcfbe34fd96b1abbd6d133
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2023-02-15 10:52:53 +0100

    view_qualtricsActions change

--- server/db/v1.3.0.sql
+++ server/db/v1.3.0.sql
@@ -56,6 +56,39 @@ INSERT IGNORE INTO `sections_hierarchy` (`parent`, `child`, `position`) VALUES
 
 CALL add_table_column('qualtricsSurveys', 'id_qualtricsProjects', 'INT(10) UNSIGNED ZEROFILL NOT NULL');
 
+
+DROP VIEW IF EXISTS view_qualtricsActions;
+CREATE VIEW view_qualtricsActions
+AS
+SELECT st.id as id, st.name as action_name, s.id_qualtricsProjects as project_id, p.name as project_name, p.api_mailing_group_id,
+st.id_qualtricsSurveys as survey_id, s.qualtrics_survey_id, s.name as survey_name, s.id_qualtricsSurveyTypes, s.group_variable, typ.lookup_value as survey_type, typ.lookup_code as survey_type_code,
+id_qualtricsProjectActionTriggerTypes, trig.lookup_value as trigger_type, trig.lookup_code as trigger_type_code,
+GROUP_CONCAT(DISTINCT g.name SEPARATOR '; ') AS `groups`, 
+GROUP_CONCAT(DISTINCT g.id*1 SEPARATOR ', ') AS id_groups, 
+GROUP_CONCAT(DISTINCT l.lookup_value SEPARATOR '; ') AS functions,
+GROUP_CONCAT(DISTINCT l.lookup_code SEPARATOR ';') AS functions_code,
+GROUP_CONCAT(DISTINCT l.id SEPARATOR '; ') AS id_functions,
+schedule_info, st.id_qualtricsActionScheduleTypes, action_type.lookup_code as action_schedule_type_code, action_type.lookup_value as action_schedule_type, id_qualtricsSurveys_reminder, 
+CASE 
+	WHEN action_type.lookup_value = 'Reminder' THEN s_reminder.name 
+    ELSE NULL
+END as survey_reminder_name, st.id_qualtricsActions
+FROM qualtricsActions st 
+INNER JOIN qualtricsSurveys s ON (st.id_qualtricsSurveys = s.id)
+INNER JOIN qualtricsProjects p ON (s.id_qualtricsProjects = p.id)
+INNER JOIN lookups typ ON (typ.id = s.id_qualtricsSurveyTypes)
+INNER JOIN lookups trig ON (trig.id = st.id_qualtricsProjectActionTriggerTypes)
+INNER JOIN lookups action_type ON (action_type.id = st.id_qualtricsActionScheduleTypes)
+LEFT JOIN qualtricsSurveys s_reminder ON (st.id_qualtricsSurveys_reminder = s_reminder.id)
+LEFT JOIN qualtricsActions_groups sg on (sg.id_qualtricsActions = st.id)
+LEFT JOIN `groups` g on (sg.id_groups = g.id)
+LEFT JOIN qualtricsActions_functions f on (f.id_qualtricsActions = st.id)
+LEFT JOIN lookups l on (f.id_lookups = l.id)
+GROUP BY st.id, st.name, s.id_qualtricsProjects, p.name,
+st.id_qualtricsSurveys, s.name, s.id_qualtricsSurveyTypes, typ.lookup_value, 
+id_qualtricsProjectActionTriggerTypes, trig.lookup_value;
+
+
 UPDATE qualtricsSurveys
 SET id_qualtricsProjects = (SELECT project_id FROM view_qualtricsActions WHERE survey_id = qualtricsSurveys.id LIMIT 0, 1)
 WHERE id_qualtricsProjects = NULL;
@@ -120,36 +153,5 @@ FROM qualtricsSurveys s
 INNER JOIN qualtricsProjects p ON (s.id_qualtricsProjects = p.id)
 INNER JOIN lookups typ ON (typ.id = s.id_qualtricsSurveyTypes);
 
-
-DROP VIEW IF EXISTS view_qualtricsActions;
-CREATE VIEW view_qualtricsActions
-AS
-SELECT st.id as id, st.name as action_name, s.id_qualtricsProjects as project_id, p.name as project_name, p.api_mailing_group_id,
-st.id_qualtricsSurveys as survey_id, s.qualtrics_survey_id, s.name as survey_name, s.id_qualtricsSurveyTypes, s.group_variable, typ.lookup_value as survey_type, typ.lookup_code as survey_type_code,
-id_qualtricsProjectActionTriggerTypes, trig.lookup_value as trigger_type, trig.lookup_code as trigger_type_code,
-GROUP_CONCAT(DISTINCT g.name SEPARATOR '; ') AS `groups`, 
-GROUP_CONCAT(DISTINCT g.id*1 SEPARATOR ', ') AS id_groups, 
-GROUP_CONCAT(DISTINCT l.lookup_value SEPARATOR '; ') AS functions,
-GROUP_CONCAT(DISTINCT l.lookup_code SEPARATOR ';') AS functions_code,
-GROUP_CONCAT(DISTINCT l.id SEPARATOR '; ') AS id_functions,
-schedule_info, st.id_qualtricsActionScheduleTypes, action_type.lookup_code as action_schedule_type_code, action_type.lookup_value as action_schedule_type, id_qualtricsSurveys_reminder, 
-CASE 
-	WHEN action_type.lookup_value = 'Reminder' THEN s_reminder.name 
-    ELSE NULL
-END as survey_reminder_name, st.id_qualtricsActions
-FROM qualtricsActions st 
-INNER JOIN qualtricsSurveys s ON (st.id_qualtricsSurveys = s.id)
-INNER JOIN qualtricsProjects p ON (s.id_qualtricsProjects = p.id)
-INNER JOIN lookups typ ON (typ.id = s.id_qualtricsSurveyTypes)
-INNER JOIN lookups trig ON (trig.id = st.id_qualtricsProjectActionTriggerTypes)
-INNER JOIN lookups action_type ON (action_type.id = st.id_qualtricsActionScheduleTypes)
-LEFT JOIN qualtricsSurveys s_reminder ON (st.id_qualtricsSurveys_reminder = s_reminder.id)
-LEFT JOIN qualtricsActions_groups sg on (sg.id_qualtricsActions = st.id)
-LEFT JOIN `groups` g on (sg.id_groups = g.id)
-LEFT JOIN qualtricsActions_functions f on (f.id_qualtricsActions = st.id)
-LEFT JOIN lookups l on (f.id_lookups = l.id)
-GROUP BY st.id, st.name, s.id_qualtricsProjects, p.name,
-st.id_qualtricsSurveys, s.name, s.id_qualtricsSurveyTypes, typ.lookup_value, 
-id_qualtricsProjectActionTriggerTypes, trig.lookup_value;
 
 
