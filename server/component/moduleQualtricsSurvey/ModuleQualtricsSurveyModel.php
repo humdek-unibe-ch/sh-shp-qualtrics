8810ef97ef9e66c3bcb919f1395903d5d96267f0
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2023-04-05 17:56:40 +0200

    check qualtricsk api key

--- server/component/moduleQualtricsSurvey/ModuleQualtricsSurveyModel.php
+++ server/component/moduleQualtricsSurvey/ModuleQualtricsSurveyModel.php
@@ -1076,7 +1076,7 @@ class ModuleQualtricsSurveyModel extends ModuleQualtricsModel
      */
     public function get_user_qualtrics_api_key($id_users = null)
     {
-        if (!isset($this->user_qualtrics_api_key)) {
+        if (!isset($this->user_qualtrics_api_key) || $this->user_qualtrics_api_key == "") {
             // check the database only once. If it is already assigned do not make a query and just returned the already assigned value
             $form_id = $this->user_input->get_form_id(QUALTRICS_SETTINGS, FORM_INTERNAL);
             if ($form_id) {
